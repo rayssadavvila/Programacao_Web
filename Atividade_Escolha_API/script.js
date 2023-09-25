531fb511d96fa5f536ea99e636becf9186839e1b
rayssadavvila <rayssamunizd@gmail.com>
2023-09-25 23:57:03 +0000

    ANother test

--- Atividade_Escolha_API/script.js
+++ Atividade_Escolha_API/script.js
@@ -3,13 +3,12 @@ const listaAtividades = document.getElementById("listaAtividades");
 const exibirLista = (lista) => {
     listaPessoas.innerHTML = "";
     for (let i = 0; i < lista.length; ++i) {
-      const li = document.createElement("li");
-      listaPessoas.start = contIndex;
-      const text = document.createTextNode(
-        `${lista[i].name} (${lista[i].birth_year}) (${lista[i].height} cm)`
-      );
-      li.appendChild(text);
-      listaPessoas.appendChild(li);
+    const li = document.createElement("li");
+    const link = document.createElement("a");
+    link.href = lista[i].link;
+    link.textContent = `${lista[i].activity} (${lista[i].type}) (${lista[i].participants}) (${lista[i].price}) (${lista[i].accessibility})`;
+    li.appendChild(link);
+    listaAtividades.appendChild(li);
     }
   };
 
